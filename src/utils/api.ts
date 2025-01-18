0425e3a5cc0142fa2e51d9be7e064e6f4864c537
pavel7991 <pasha.selin97@gmail.com>
2025-01-19 00:18:34 +0200

    update https

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const fetchData = async (name: string) => {
   try {
-    const response = await axios.get(`http://www.omdbapi.com/?apikey=e70e8a97&s=${name}`)
+    const response = await axios.get(`https://www.omdbapi.com/?apikey=e70e8a97&s=${name}`)
     return response.data
   } catch (error) {
     if (axios.isAxiosError(error)) {
